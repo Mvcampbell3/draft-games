14d5bf9d5fcd4fd19161eb587e86834c03f67778
Mvcampbell3 <mvcampbell3@gmail.com>
2021-09-25 15:14:29 -0400

    react with app and landing page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Pages
+import { LandingPage } from './components/pages';
+
+const App = () => {
+    return ( 
+        <div className="app-container">
+            <h1>This is the app container</h1>
+            <LandingPage />
+        </div>
+     );
+}
+ 
+export default App;
---
+++ src/components/pages/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./LandingPage.scss";
+
+const LandingPage = () => {
+    return ( 
+        <div className="page-container">
+            <h1>This is the landing page</h1>
+        </div>
+     );
+}
+ 
+export default LandingPage;
