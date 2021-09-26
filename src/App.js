881b2265ba2d90fd5935b5cbe7d1009c83761137
Mvcampbell3 <mvcampbell3@gmail.com>
2021-09-26 14:09:18 -0400

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
 // Pages
 import { LandingPage } from './components/pages';
 
+// firebase
+import firebase from './firebase';
+
 const App = () => {
+
+    const test = firebase.getApps();
+    console.log({test: test[0]._options})
+
+
     return ( 
         <div className="app-container">
             <h1>This is the app container</h1>
