0d49e6459d8be0fdd66d61db2bee8d5937ca50fb
Mvcampbell3 <mvcampbell3@gmail.com>
2022-01-23 22:28:09 -0500

    formik wrapped Input component

--- src/components/common/Input/Input.js
+++ src/components/common/Input/Input.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./Input.scss";
+import { useField } from "formik";
 
 import { Form } from "react-bulma-components";
 
-const Input = ({ label = "", id = "", ...props }) => {
+const Input = ({ label = "", id = "", type = "field", ...props }) => {
+    const [field, meta] = useField(props);
+    const hasError = meta.error && meta.touched;
+    const renderInput = (type) => {
+        switch (type) {
+            case "select": {
+                const { children } = props;
+                return (
+                    <Form.Select id={id} {...props} {...field}>
+                        {children}
+                    </Form.Select>
+                );
+            }
+            case "textarea":
+                return <Form.Textarea id={id} {...props} {...field} />;
+            case "field":
+            default:
+                return (
+                    <Form.Input
+                        id={id}
+                        {...props}
+                        {...field}
+                        color={hasError ? "danger" : ""}
+                    />
+                );
+        }
+    };
     return (
         <Form.Field className="mb-4">
-            <label htmlFor={id}>{label}</label>
-            <Form.Control>
-                <Form.Input id={id} {...props} />
-            </Form.Control>
+            <Form.Label htmlFor={id}>{label}</Form.Label>
+            <Form.Control>{renderInput(type)}</Form.Control>
+            {meta.error && meta.touched && (
+                <Form.Help color="danger">{meta.error}</Form.Help>
+            )}
         </Form.Field>
     );
 };
